87174f5ec4bafb64fb3363f93e7ed06d38f519b8
DJ <gk22719@gmail.com>
2025-05-11 06:27:53 +0530

    added fetching and data loading features for the frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,15 @@
-import Image from "next/image";
+
+import { HomePage } from "@/pages/HomePage";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">
-        This is a simple Next.js application.
-      </p>
-      <Image
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
-    </main>
-  );
+	return (
+		<div>
+			<main className="px-4 max-w-[90rem] mx-auto w-full mt-10">
+				<h1 className="text-4xl font-bold text-left w-full">
+					Upcoming Events in Australia
+				</h1>
+				<HomePage />
+			</main>
+		</div>
+	);
 }
